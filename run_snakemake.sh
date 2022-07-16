4905486184a8423e0ce309d75b7ca0ef7acb5ad9
Pierre Poulain <pierre.poulain@cupnet.net>
2022-07-16 17:55:19 +0200

    Use snakemake profile

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -12,6 +12,12 @@ module load snakemake
 module load slurm-drmaa
 
 # --jobs=xx is the maximum number of cpus used simultaneously
-snakemake --cluster-config cluster.yml \
-    --drmaa " --mem={cluster.mem} --cpus-per-task={cluster.cpus}" \
-    --use-conda --jobs "${NJOBS}" --latency-wait 20 --keep-going
+#snakemake --cluster-config cluster.yml \
+#    --drmaa " --mem={cluster.mem} --cpus-per-task={cluster.cpus}" \
+#    --use-conda --jobs "${NJOBS}" --latency-wait 20 --keep-going
+
+
+snakemake --profile snakemake_profiles/cluster/ \
+    --jobs "${NJOBS}" \
+    --drmaa " --mem={resources.mem_mb} --cpus-per-task={threads}"
+
