2d440a0d7b689f442ca058c2785d54a8132dcc99
Pierre Poulain <pierre.poulain@cupnet.net>
2022-07-16 18:39:58 +0200

    Define path

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -3,20 +3,17 @@
 # Script will stop at the first error
 set -euo pipefail
 
-
-# Parameters
+# Maximum number of cpus used simultaneously
 NJOBS=50
 
 # Load modules
 module load snakemake
 module load slurm-drmaa
 
-# --jobs=xx is the maximum number of cpus used simultaneously
-#snakemake --cluster-config cluster.yml \
-#    --drmaa " --mem={cluster.mem} --cpus-per-task={cluster.cpus}" \
-#    --use-conda --jobs "${NJOBS}" --latency-wait 20 --keep-going
+# Paths
+export DATA_DIR="/shared/projects/form_2021_29/data/rnaseq_scere"
 
-
+# Run snakemake with cluster mode
 snakemake --profile snakemake_profiles/cluster/ \
     --jobs "${NJOBS}" \
     --drmaa " --mem={resources.mem_mb} --cpus-per-task={threads}"
